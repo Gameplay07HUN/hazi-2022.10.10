27ae44c10cc0b143da546ca04c506d567ae91378
Gameplay07HUN <113524179+Gameplay07HUN@users.noreply.github.com>
2022-10-10 09:08:59 +0200

    cucc

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Vasmegye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] beolvasoscucc = File.ReadAllLines("vas.txt");
+            Console.WriteLine(beolvasoscucc.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
